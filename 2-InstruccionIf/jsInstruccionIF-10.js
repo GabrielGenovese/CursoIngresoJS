4895b7e337f2b147cdbb9a2c095570fe185d9d27
Gabriel <gabrielgenovese97@gmail.com>
2021-02-09 22:58:24 -0300

    TP4 completo y arreglos

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,14 +1,30 @@
 //Nombre: Genovese Gabriel
 //Ejercicio 10
 
+//Solo tomar los extremos.
+/*Test
+notaExamen = 9 EXCELENTE
+notaExamen = 8 APROBO
+notaExamen = 4 APROBO
+notaExamen = 2 Vamos, la proxima se puede
+*/
+
 function mostrar()
 {
 	let notaExamen;
+
 	//Genero el número RANDOM entre 1 y 10 
+
 	notaExamen = Math.floor(Math.random() * 10);
 
 
-	if (notaExamen == 10 || notaExamen == 9)
+	//Hardcodeo
+	//notaExamen = 9;
+	//notaExamen = 8;
+	//notaExamen = 4;
+	//notaExamen = 3;
+
+	if (notaExamen == 10 || notaExamen == 9) // es mas eficiente(notaExamen > 8)
 	{
 		alert("EXCELENTE");
 	}
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,97 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
+
+ //Nombre: Gabriel Genovese, Div H
+ // TP 4
+
 function CalcularPrecio () 
 {
- 	
+    let cantidadLamparas;
+    let precioInicial;
+    let marca;
+    let descuento;
+    let precioConDescuento;
+    let IIBB;
+
+
+    cantidadLamparas = document.getElementById("txtIdCantidad").value;
+    marca = document.getElementById("Marca").value;
+
+
+    cantidadLamparas = parseInt(cantidadLamparas);
+
+    precioInicial = cantidadLamparas * 35;
+
+
+    if (cantidadLamparas > 5)
+    {
+        descuento = precioInicial * 0.5;
+    }
+    else
+    {
+        if (cantidadLamparas == 5 )
+        {
+            if (marca == "ArgentinaLuz")
+            {
+                descuento = precioInicial * 0.4;
+            }
+            else
+            {
+                descuento = precioInicial * 0.3;
+            }
+        }
+        else 
+        {
+            if(cantidadLamparas == 4)
+            {
+                if (marca == "ArgentinaLuz" || marca == "FelipeLamparas")
+                {
+                    descuento = precioInicial * 0.25;
+                }
+                else
+                {
+                    descuento = precioInicial * 0.2;
+                }
+            }
+            else
+            {
+                if (cantidadLamparas == 3)
+                {
+                    if (marca == "ArgentinaLuz")
+                    {
+                        descuento = precioInicial * 0.15;
+                    }
+                    else
+                    {
+                        if (marca == "FelipeLamparas")
+                        {
+                            descuento = precioInicial * 0.1;
+                        }
+                        else
+                        {
+                            descuento = precioInicial * 0.5;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    precioConDescuento = precioInicial - descuento;
+
+    if (precioConDescuento > 120)
+    {
+        IIBB = precioConDescuento * 0.1;
+        precioConDescuento += IIBB;
+        alert("Usted pago "+ IIBB +" de IIBB");
+    }
+    
+    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+    
 }
+
+    //Todo lo que esta en comentario en los if es inecesario, 
+    //ya que solo hace falta dejar la variable q cambia. 
+    //Ej: en "descuento" solo cambia el numero a multiplicar
+    
